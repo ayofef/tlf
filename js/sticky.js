0e8f1998eaf6f81e7bedc9b6e666201bd0aa2395
Ayofe Farouq <ayofef@gmail.com>
2020-02-25 00:29:53 +0000

    25-02-12-29

--- js/sticky.js
+++ js/sticky.js
@@ -5,12 +5,10 @@ $(document).ready(function() {
     
       if ($(window).scrollTop() > 720) {
         $('#navbar').addClass('navbar-fixed');
-        $('#nav-box').addClass('container');
         
       }
       if ($(window).scrollTop() < 721) {
         $('#navbar').removeClass('navbar-fixed');
-        $('#nav-box').removeClass('container');
       }
     });
   });
